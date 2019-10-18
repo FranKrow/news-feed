68ebc99956280bb129251ce0bf5973c8eeac06af
Daniel Franco <daniel.alberto.franco@delphi.com>
2019-10-18 08:35:37 -0500

    Changing debate tags

--- src/data/debate.js
+++ src/data/debate.js
@@ -5,12 +5,12 @@ const SOURCE = {
   url: 'http://www.debate.com.mx',
   map: {
     addUrlToPath: true,
-    container: '.col__main article',
-    title: { find: '.news__media>a', attr: 'title' },
-    prev: { find: '.news__excerpt p' },
+    container: '.row__container article',
+    title: { find: '.news__title>a' },
+    prev: { find: '.news__data a' },
     link: { find: '.news__media>a', attr: 'href' },
     datePost: { find: '.news__time', attr: 'datetime' },
-    img: { find: '.news__media>a>img', attr: 'src' }
+    img: { find: '.news__media>a img', attr: 'src' }
   }
 };
 
