02c31e6b377b053d7f6e8f794fb39645d7274614
Franco Daniel <isc.franco.daniel@gmail.com>
2020-04-22 13:40:05 -0500

    Adding the url to the source

--- src/data/debate.js
+++ src/data/debate.js
@@ -4,21 +4,21 @@ const SOURCE = {
   name: 'DEBATE',
   url: 'http://www.debate.com.mx',
   map: {
-    addUrlToPath: false,
+    addUrlToPath: true,
     addUrlToIMG: true,
     container: '.row__container article',
     title: { find: '.news__title>a' },
     prev: { find: '.news__data a' },
     link: { find: '.news__media>a', attr: 'href' },
     datePost: { find: '.news__time', attr: 'datetime' },
-    img: { find: '.news__media>a img', attr: 'src' }
-  }
+    img: { find: '.news__media>a img', attr: 'src' },
+  },
 };
 
 const dataProvider = {
   getNews: async () => {
     return baseDataProvider.getNews(SOURCE);
-  }
+  },
 };
 
 module.exports = dataProvider;
