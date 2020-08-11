47ece9276e15e6eb0e2d9eb188f88967c4ab3a24
Franco Daniel <daniel.alberto.franco@aptiv.com>
2020-08-11 13:30:38 -0500

    Changed source for el universal

--- src/data/universal.js
+++ src/data/universal.js
@@ -6,12 +6,12 @@ const SOURCE = {
   map: {
     addUrlToPath: true,
     addUrlToIMG: false,
-    container: 'div.multimedia',
-    title: { find: '.field-content>a img', attr: 'title' },
-    prev: { find: '.field-content.slideinfos' },
-    link: { find: '.field-content>a', attr: 'href' },
-    datePost: { find: '.views-field-created>.field-content' },
-    img: { find: '.field-content>a img', attr: 'src' },
+    container: 'article.type-article',
+    title: { find: '.ce6-Tipo1_Titulo>a' },
+    prev: { find: '.ce6-Tipo1_Titulo>a' },
+    link: { find: '.ce6-Tipo1_Titulo>a', attr: 'href' },
+    datePost: { find: '.ce6-Tipo1_Hora' },
+    img: { find: '.ce6-Tipo1_Imagen:first-of-type', attr: 'data-src' },
   },
 };
 
