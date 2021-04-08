c9fd1a04715630ae9bd35a3b178cc86641c32e92
Franco Daniel <dafc14@hotmail.com>
2021-04-08 08:41:02 -0500

    Update news.js
    Removing el universal bcs some issues

--- src/models/news.js
+++ src/models/news.js
@@ -2,7 +2,7 @@ const unocero = require('./../data/unocero');
 const debate = require('./../data/debate');
 const asisucede = require('./../data/asisucede');
 const quadratin = require('./../data/cuadratin');
-const universal = require('./../data/universal');
+//const universal = require('./../data/universal');
 const ligamx = require('./../data/ligamx');
 
 const Model = {
@@ -12,7 +12,7 @@ const Model = {
       debate.getNews(),
       asisucede.getNews(),
       quadratin.getNews(),
-      universal.getNews(),
+      //universal.getNews(),
       ligamx.getNews(),
     ]).then(function (values) {
       return values;
